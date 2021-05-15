879d0dd5f390fa5510d3aa5fcc4c269b09ed5c5a
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-15 20:21:17 -0300

    Add - url e view de exemplo

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -13,3 +14,12 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, 'hello_world.html', {'title': 'Contact us'})
+
+
+def example_page(request):
+    context = {'title': 'Example'}
+    template_name = 'hello_world.html'
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+
+    return HttpResponse(rendered_item)
