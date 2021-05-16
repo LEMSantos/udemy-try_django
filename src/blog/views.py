f5d93dadcf23144cd4da15d61a38b302b2980a5a
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-16 15:58:19 -0300

    Add - função para resolver a url do blog

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import BlogPost
+
+
+def blog_post_detail_page(request):
+    blog_post = BlogPost.objects.first()
+
+    template_name = 'blog_post_detail.html'
+    context = {'blog_post': blog_post}
+
+    return render(request, template_name, context)
