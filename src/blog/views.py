73acde5a92b50603fd982c7276676806062021b7
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-16 16:32:26 -0300

    Add - tratamento de erros e recuperação dos posts com o slug

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.db.models import query
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
 
-def blog_post_detail_page(request):
-    blog_post = BlogPost.objects.first()
+def blog_post_detail_page(request, slug):
+    queryset = BlogPost.objects.filter(slug=slug)
+
+    if queryset.count() == 0:
+        raise Http404
+
+    blog_post = queryset.first()
 
     template_name = 'blog_post_detail.html'
     context = {'blog_post': blog_post}
