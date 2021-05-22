9cad1ac827e406dbf919f39b39a0f3669d651440
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-22 10:48:52 -0300

    Md - de TextField para CharField no model do Post

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class BlogPost(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField(null=True, blank=True)
     slug = models.SlugField(unique=True)
