161194e1202a5fbb335be56e8ca2a721e73eb435
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-30 09:37:55 -0300

    Add - queryset para pegar somente posts publicados

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,8 +1,23 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
+
+
+class BlogPostQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(publish_date__lte=now)
+
+
+class BlogPostManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return BlogPostQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
 
 
 class BlogPost(models.Model):
@@ -17,6 +32,8 @@ class BlogPost(models.Model):
     )
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = BlogPostManager()
 
     class Meta:
         ordering = ['-publish_date', '-updated', '-timestamp']
