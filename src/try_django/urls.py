5607464e114fcfaf000f06192508b52e2c81184d
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-30 10:12:16 -0300

    Add - urls para as medias e arquivos estáticos

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,12 +24,20 @@ from .views import home_page, about_page, contact_page, example_page
 
 urlpatterns = [
     path('', home_page),
-
     path('blog-new/', blog_post_create_view),
     path('blog/', include('blog.urls')),
-
     path('about/', about_page),
     path('contact/', contact_page),
     path('example/', example_page),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
