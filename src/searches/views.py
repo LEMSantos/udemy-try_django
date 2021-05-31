60c7729ff33df464462eec4527bd3c58a51cc575
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-31 19:55:11 -0300

    Add - view de busca do projeto

--- src/searches/views.py
+++ src/searches/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import SearchQuery
+
+
+def search_view(request):
+    template_name = 'searches/view.html'
+    query = request.GET.get('q', None)
+    user = None
+
+    if request.user.is_authenticated:
+        user = request.user
+
+    if query is not None:
+        SearchQuery.objects.create(user=user, query=query)
+
+    context = {'query': query}
+
+    return render(request, template_name, context)
